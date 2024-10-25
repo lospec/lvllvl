fbf8c2b5650be7641bf2f9e1629bd304e51b818c
Sam Keddy <skeddy13@yahoo.com>
2024-10-25 15:15:13 -0400

    added trixel 4px

--- src/js/textMode/tileSet/characterSetPresets.js
+++ src/js/textMode/tileSet/characterSetPresets.js
@@ -198,7 +198,8 @@ var CharacterSetPresets = [
   {
 	category: "Trixel Art",
 	characterSets: [
-		{ "name": "Trixel 3px", "id": "trixel_3px", width: 4, height: 3, author: "skeddles", authorlink: ' (<a href="https://lospec.com/skeddles" target="_blank">https://lospec.com/skeddles</a>)', type: "custom" },
+		{ "name": "Trixel 4x3", "id": "trixel_3px", width: 4, height: 3, author: "skeddles", authorlink: ' (<a href="https://lospec.com/skeddles" target="_blank">https://lospec.com/skeddles</a>)', type: "custom" },
+		{ "name": "Trixel 5x4", "id": "trixel_4px", width: 4, height: 3, author: "skeddles", authorlink: ' (<a href="https://lospec.com/skeddles" target="_blank">https://lospec.com/skeddles</a>)', type: "custom" },
 	]
   },
   {
